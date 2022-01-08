50929f5bdc8b329773b034ca4ebedb5925b163a2
AnnaDorosheva <annador@ukr.net>
2022-01-08 15:25:22 +0200

    add Local Storage for todos

--- src/components/TodoApp/TodoApp.js
+++ src/components/TodoApp/TodoApp.js
@@ -1,18 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import s from "./TodoApp.module.css";
 import TodoEditor from "./TodoEditor/TodoEditor";
 import TodoList from "./TodoList/TodoList";
 
 const Todo = () => {
+  
   // todo list
   const [todos, setTodos] = useState([]);
   const addTodos = (todo) => {
     setTodos((prevTodos) => [...prevTodos, todo]);
   };
+
   // delet dodo
   const deleteTodo = (todoId) => {
     setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== todoId));
   };
+
+  // Local Storage
+  useEffect(() => {
+const persistedTodos = localStorage.getItem("todos");
+if(persistedTodos) {
+  setTodos(JSON.parse(persistedTodos));
+}
+  }, []);
+
+  useEffect(() => {
+localStorage.setItem("todos", JSON.stringify(todos))
+  }, [todos]);
+
   return (
     <div className={s.todoContainer}>
       <h2>Todo App ( Приложение для планирования задач )</h2>
